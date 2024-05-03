9fd897855068b830c5a9a15f5123a9531cc82cb4
Viktor <vvlasovdo@mail.ru>
2024-05-03 09:51:30 +0900

    ref: rename reducer

--- src/hooks/use-test-modal/index.ts
+++ src/hooks/use-test-modal/index.ts
@@ -2,7 +2,7 @@ import { useAppDispatch, useAppSelector } from "store/hook";
 import { testModalSlice } from "store/reducers/test-modal/test-modal-slice";
 
 export const useTestModal = () => {
-    const { isOpen } = useAppSelector(state => state.questionModalSlice);
+    const { isOpen } = useAppSelector(state => state.testModalSlice);
     const dispatch = useAppDispatch();
 
     const openModal = () => dispatch(testModalSlice.actions.openModal());
