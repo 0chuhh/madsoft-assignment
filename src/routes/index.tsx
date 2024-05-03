8be42620043155deab590923cfdda0e25d84d299
Viktor <vvlasovdo@mail.ru>
2024-05-03 09:50:12 +0900

    feat: add test list and test detail routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { AuthLayout } from "components/layout/auth";
 import AuthPage from "pages/auth";
-import { Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+const TestListPage = lazy(() => import("pages/tests/"));
+const TestDetailPage = lazy(() => import("pages/tests/detail/"));
 
 
 export const Routing = () => {
@@ -8,10 +11,10 @@ export const Routing = () => {
 		<Routes>
 			<Route path="/sign-in" element={<AuthPage />} />
 			<Route element={<AuthLayout />}>
-				<Route path="/" element={<div>hello</div>}>
-				</Route>
-				
+				<Route path="/tests" element={<TestListPage />} />
+				<Route path="/tests/:testId" element={<TestDetailPage />} />
 			</Route>
+			<Route path="/*" element={<Navigate to={'/tests'}/>} />
 		</Routes>
 	);
 };
