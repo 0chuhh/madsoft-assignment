dcb08aee78ee2b48c5768be5f29ef0da47d92bc8
Viktor <vvlasovdo@mail.ru>
2024-05-03 09:49:40 +0900

    feat: add test and test modal reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,20 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import userReducer from './reducers/user/user-slice'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from './reducers/user/user-slice';
+import currentTestSlice from "./reducers/current-test/current-test-slice";
+import testModalSlice from "./reducers/test-modal/test-modal-slice";
 
 const rootReducer = combineReducers({
-    userReducer
-})
+    userReducer,
+    currentTestSlice,
+    testModalSlice
+});
 
 export const setupStore = () => {
     return configureStore({
         reducer: rootReducer
-    })
-}
+    });
+};
 
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
