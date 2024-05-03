e6f6e794ebb8982b58d21d873a6df103e71fdaa9
Viktor <vvlasovdo@mail.ru>
2024-05-03 09:37:02 +0900

    feat: use test modal hook

---
+++ src/hooks/use-test-modal/index.ts
@@ -0,0 +1,15 @@
+import { useAppDispatch, useAppSelector } from "store/hook";
+import { testModalSlice } from "store/reducers/test-modal/test-modal-slice";
+
+export const useTestModal = () => {
+    const { isOpen } = useAppSelector(state => state.questionModalSlice);
+    const dispatch = useAppDispatch();
+
+    const openModal = () => dispatch(testModalSlice.actions.openModal());
+
+    const closeModal = () => dispatch(testModalSlice.actions.closeModal());
+
+    const toggleModal = () => dispatch(testModalSlice.actions.closeModal());
+
+    return { isOpenModal: isOpen, openModal, closeModal, toggleModal };
+};
